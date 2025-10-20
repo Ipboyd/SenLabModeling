78087e065d345c4633bbe4c69d3c96dabdfa9150
Ipboyd <ipboyd@bu.edu>
2025-10-20 11:18:12 -0400

    Project Outline
    The following push outlines many of the files needed in order to rebuild our network in python.

---
+++ pytoc/declarations.py
@@ -0,0 +1,19 @@
+#This file serves as a user "front-end"
+#The user will declare their nodes and the connectivity between these nodes (synapses).
+#This architecture will then be interpreted by the dfs->euler_compiler to build the solve file
+
+#------------------------------------------------------------------#
+# Please first declared your nodes and thier respective properties #
+#------------------------------------------------------------------#
+# Description: Within this area, you can create neurons by calling the function Neuron.
+
+
+Onset_input = 
+
+
+
+
+
+#---------------------------------------------------------------------#
+# Next, please declare your synapses and respective synapse properies #
+#---------------------------------------------------------------------#
