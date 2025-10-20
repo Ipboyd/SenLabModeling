16353d82339ff525d0552ae5f90d892c7c3078ef
Ipboyd <ipboyd@bu.edu>
2025-10-20 14:58:33 -0400

    Small update

--- pytoc/declarations.py
+++ pytoc/declarations.py
@@ -6,14 +6,24 @@
 # Please first declared your nodes and thier respective properties #
 #------------------------------------------------------------------#
 # Description: Within this area, you can create neurons by calling the function Neuron.
+#              After all nuerons have been declared you must wrap all of the neurons
+#              within a dictionary to be used downstream
 
+from mechs import Lif_Neuron
+from mechs import Lif_Synapse
+from BuildGraph import Forwards_Method
 
-Onset_input = 
+Onset_input = Lif_Neuron.Build_Vars(name = 'On')
+Relay_1 = Lif_Neuron.Build_Vars(name = 'ROn')
 
+neurons = {'neurons' : [Onset_input, Relay_1]}
 
-
-
+print(neurons)
 
 #---------------------------------------------------------------------#
 # Next, please declare your synapses and respective synapse properies #
 #---------------------------------------------------------------------#
+# Convention : Pre Node  ->  Post Node   Ex. On_ROn 
+On_R1_synpase = Lif_Synapse.Build_Vars(name = 'On_ROn')
+
+print(On_R1_synpase)
