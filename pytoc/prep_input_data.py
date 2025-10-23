9c1a302e685ab61d9db6c88773271055f969cafc
Ram Zaveri <ramzaveri2001@gmail.com>
2025-10-23 15:45:53 -0400

    updated prep input

--- pytoc/prep_input_data.py
+++ pytoc/prep_input_data.py
@@ -1,86 +1,146 @@
 import numpy as np
+import scipy.io
 from tqdm import tqdm
+from argparse import ArgumentParser
 
-def genSpatiallyTunedChans(azi, chans=19, sigma=300):
-    
-    spatialCurves = np.zeros((chans,chans))
-    for idx in range(chans): 
-        spatialCurves[idx,:]= np.exp(((-1/2)*(azi-azi[idx])**2)*(1/sigma)) 
+class PrepInput(object):
+    '''
+    Class to prepare input data for IC neuron simulations based on STRF data.
+    Attributes:
+        chans : int
+            Number of channels.
+        azi : np.ndarray
+            Azimuth locations for channels. 
+        sigma : float
+            Standard deviation for Gaussian tuning curves.
+        trials : int
+            Number of trials.   
+        padToTime : int
+            Time to pad spike trains to (ms).
+        dt : float      
+            Time step in ms.
         
-    return spatialCurves
+    '''
+    def __init__(self, args):
+        
+        self.chans = args.chans
+        self.sigma = args.sigma
+        self.trials = args.trials
+        self.padToTime = args.padToTime
+        self.dt = args.dt
+        
+        self.azi = np.flip(np.linspace(-90,90, self.chans))
+        
+        self.spatialCurves = self.genSpatiallyTunedChans()
+
+    def genSpatiallyTunedChans(self):
+        '''
+        Generate spatial tuning curves for each channel based on Gaussian profiles.
+        '''
+        spatialCurves = np.zeros((self.chans, self.chans))
+        for idx in range(self.chans): 
+            spatialCurves[idx,:]= np.exp(((-1/2)*(self.azi-self.azi[idx])**2)*(1/self.sigma)) 
+            
+        return spatialCurves
+
+    def make_grid_target_masker_locs(self):
+        '''
+        Create grid (coordinates) of masker and target locations based on number of channels.
+        '''
+        grid_x, grid_y = np.meshgrid(np.arange(len(self.azi)), np.arange(len(self.azi))) # masker, target
+        masker_locs = grid_x.flatten() # masker
+        target_locs = grid_y.flatten() # target
+        return masker_locs, target_locs  
+
+    def gen_IC_spks(self, tmax, locs, fr_targets, fr_masker, newStrfGain, strfGain):
+
+        m_loc, t_loc = locs # masker, target
+        singleConfigSpks = np.zeros((self.trials,self.spatialCurves.shape[0], tmax))  
+        
+        for t in range(self.trials):
+            for ch in range(self.spatialCurves.shape[0]):
+                t_wt = self.spatialCurves[ch,self.azi==self.azi[t_loc]] if t_loc is not None else 0.
+                m_wt = self.spatialCurves[ch,self.azi==self.azi[m_loc]] if m_loc is not None else 0.
+
+                if t_wt + m_wt == 0: raise Exception(f'No contribution from target or masker at channel {ch}, trial {t+1}. Both cannot be None.')
+                    
+                singleConfigSpks[t,ch,:] = t_wt*fr_targets.squeeze() + m_wt*fr_masker[t].squeeze()      
+
+                if t_wt + m_wt >= 1: singleConfigSpks[t,ch,:] = singleConfigSpks[t,ch,:] / (t_wt + m_wt)
+        
+        if singleConfigSpks.shape[2] < self.padToTime/self.dt:
+            padSize = int(self.padToTime/self.dt)-singleConfigSpks.shape[2]
+            singleConfigSpks = np.concatenate([singleConfigSpks, np.zeros((self.trials,self.spatialCurves.shape[0],padSize))], axis=2)
+        
+        spks =  singleConfigSpks.transpose(2,1,0) * newStrfGain / strfGain  # Shape to (time, chans, trials)
+
+        return spks
 
 
-def make_grid_target_masker_locs(azi):
-    grid_x, grid_y = np.meshgrid(np.arange(len(azi)), np.arange(len(azi))) # masker, target
-    masker_locs = grid_x.flatten() # masker
-    target_locs = grid_y.flatten() # target
-    return masker_locs, target_locs  
+    def process_input(self, strf_path, list_locs, on_neuron=True, off_neuron=True):
+        
+        data = scipy.io.loadmat(strf_path)
+        fr_target_on = np.array([np.array(dta) for dta in data['fr_target_on'].squeeze()])
+        fr_target_off = np.array([np.array(dta) for dta in data['fr_target_off'].squeeze()])
+        fr_masker = np.array([np.array(dta) for dta in data['fr_masker'].squeeze()])
+        strfGain = float(data['strfGain'].squeeze())
+        tmax = fr_target_on.shape[1]
+        newStrfGain = strfGain
+        
+        progress_bar = tqdm(list_locs)
 
-def gen_IC_spks(spatialCurves, azi, tmax, locs, fr_targets, fr_masker, newStrfGain, strfGain, trials=1, padToTime = 3500, dt=0.1):
-    
-    m_loc, t_loc = locs
-    singleConfigSpks = np.zeros((trials,spatialCurves.shape[0],tmax))  
-    
-    for t in range(trials):
-        for ch in range(spatialCurves.shape[0]):
-            t_wt = spatialCurves[ch,azi==azi[t_loc]] if t_loc is not None else 0.
-            m_wt = spatialCurves[ch,azi==azi[m_loc]] if m_loc is not None else 0.
-
-            if t_wt + m_wt == 0: raise Exception(f'No contribution from target or masker at channel {ch}, trial {t+1}. Both cannot be None.')
+        spks_dict = {}
+        for locs in progress_bar:
+            progress_bar.set_description(f'Generating spikes for Masker Loc: {locs[0]}, Target Loc: {locs[1]}')
+            if on_neuron:
+                spks_dict[f'locs_masker_{locs[0]}_target_{locs[1]}_on'] = {}
+                for stimulus in range(fr_target_on.shape[0]):
+                    on_spks = self.gen_IC_spks(
+                                        tmax=tmax, 
+                                        locs=locs, 
+                                        fr_targets=fr_target_on[stimulus], 
+                                        fr_masker=fr_masker, 
+                                        newStrfGain=newStrfGain, 
+                                        strfGain=strfGain)
+                    spks_dict[f'locs_masker_{locs[0]}_target_{locs[1]}_on'][f'stimulus_{stimulus}'] = on_spks
                 
-            singleConfigSpks[t,ch,:] = t_wt*fr_targets.squeeze() + m_wt*fr_masker[t].squeeze()      
-
-            if t_wt + m_wt >= 1: singleConfigSpks[t,ch,:] = singleConfigSpks[t,ch,:] / (t_wt + m_wt)
-    
-    if singleConfigSpks.shape[2] < padToTime/dt:
-        padSize = int(padToTime/dt)-singleConfigSpks.shape[2]
-        singleConfigSpks = np.concatenate([singleConfigSpks, np.zeros((trials,spatialCurves.shape[0],padSize))], axis=2)
-    
-    spks =  singleConfigSpks.transpose(2,1,0) * newStrfGain / strfGain  # Shape to (time, chans, trials)
-
-    return spks
+            if off_neuron:
+                spks_dict[f'locs_masker_{locs[0]}_target_{locs[1]}_off'] = {}
+                for stimulus in range(fr_target_off.shape[0]):
+                    off_spks = self.gen_IC_spks(
+                                        tmax=tmax, 
+                                        locs=locs, 
+                                        fr_targets=fr_target_off[stimulus], 
+                                        fr_masker=fr_masker, 
+                                        newStrfGain=newStrfGain, 
+                                        strfGain=strfGain)
+                    spks_dict[f'locs_masker_{locs[0]}_target_{locs[1]}_off'][f'stimulus_{stimulus}'] = off_spks
+                
+        return spks_dict
 
 
 if __name__ == "__main__":
     
-    import matplotlib.pyplot as plt
-    import scipy.io
-    
-    chans = 19
-    azi = np.flip(np.linspace(-90, 90, chans))
-    spatialCurves = genSpatiallyTunedChans(azi, chans=chans, sigma=300)
-    
-    # plt.plot(azi, spatialCurves, label=[f'Chan {i}' for i in range(spatialCurves.shape[0])])
-    # plt.xlabel('Azimuth (degrees)')
-    # plt.show()    
-    
-    
     path = r"D:\School_Stuff\Rotation_1_Sep_Nov_Kamal_Sen\Code\MouseSpatialGrid-19-Chan\ICSimStim\default_STRF_with_offset_200k.mat"
-    data = scipy.io.loadmat(path)
     
-    fr_target_on = np.array([np.array(dta) for dta in data['fr_target_on'].squeeze()])
-    fr_target_off = np.array([np.array(dta) for dta in data['fr_target_off'].squeeze()])
-    fr_masker = np.array([np.array(dta) for dta in data['fr_masker'].squeeze()])
-    strfGain = float(data['strfGain'].squeeze())
-    tmax = fr_target_on.shape[1]
-    newStrfGain = strfGain
-    subz = np.linspace(5,int(chans*5),chans, dtype=int)
+    args = ArgumentParser()
+    args.add_argument('--chans', type=int, default=4, help='Number of channels')
+    args.add_argument('--trials', type=int, default=10, help='Number of trials')
+    args.add_argument('--padToTime', type=int, default=3500, help='Time to pad spike trains to (ms)')
+    args.add_argument('--sigma', type=int, default=300, help='Standard deviation for Gaussian tuning curves')    
+    args.add_argument('--dt', type=float, default=0.1, help='Time step in ms')
+    parsed_args = args.parse_args()
     
     
-    masker_locs, target_locs = make_grid_target_masker_locs(azi)
+    prep_input = PrepInput(parsed_args)
+    masker_locs, target_locs = prep_input.make_grid_target_masker_locs()
+    list_locs = list(zip(masker_locs, target_locs))
     
-    progress_bar = tqdm(zip(masker_locs, target_locs))
-    for m_loc, t_loc in progress_bar:
-        
-        progress_bar.set_description(f'Generating spikes for Masker Loc: {m_loc}, Target Loc: {t_loc}')
-        on_spks_stimulus_1 = gen_IC_spks(spatialCurves=spatialCurves, 
-                            azi=azi, 
-                            tmax=tmax, 
-                            locs=(m_loc, t_loc), 
-                            fr_targets=fr_target_on[0], 
-                            fr_masker=fr_masker, 
-                            newStrfGain=newStrfGain, 
-                            strfGain=strfGain, 
-                            trials=10, 
-                            padToTime = 3500, 
-                            dt=0.1)
+    spks = prep_input.process_input(
+                strf_path=path, 
+                list_locs=list_locs, 
+                on_neuron=True, 
+                off_neuron=True,)
+    
+    print("Generated spike train keys:", spks.keys())
+    
