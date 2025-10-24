0d8c5eda0b040449c297ae4aade719b22c7eeb29
Ipboyd <ipboyd@bu.edu>
2025-10-24 11:16:49 -0400

    Prototype

--- pytoc/run_main.py
+++ pytoc/run_main.py
@@ -1,7 +1,9 @@
 import set_options
 import declarations
 from BuildFile import Forwards_Method, Compile_Solve
-import prep_input_data2
+from argparse import ArgumentParser
+import numpy as np
+import prep_input_data_for_prototypeing
 
 #Set your runtime options
 opts = set_options.options()
@@ -11,7 +13,18 @@ arch = declarations.Declare_Architecture(opts)
 file_body_forwards = Forwards_Method.Euler_Compiler(arch[0],arch[1],arch[2],opts)
 #Compile a solve file (python or c++)
 solve_file = Compile_Solve.solve_file_generator(solve_file_body = file_body_forwards, cpp_gen = 0)
-#Generate external model inputs
-inputs, noise_tokens = prepare_inputs
+
+#Prep the input data    
+on_spks = prep_input_data_for_prototypeing.gen_poisson_inputs_parallel(10,label="on")
+off_spks = prep_input_data_for_prototypeing.gen_poisson_inputs_parallel(10,label="off")
+
+#Prep the noise tokens
+noise_tokens = 
+
+
+#print(on_spks)
+#print(np.shape(on_spks))
+
+#print(sum(sum(off_spks)))
 
 
