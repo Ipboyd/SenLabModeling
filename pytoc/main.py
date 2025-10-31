72b892b1db560f548e1577c71ddb3bc1bee9f294
Ram Zaveri <ramzaveri2001@gmail.com>
2025-10-31 14:19:29 -0400

    minor

--- pytoc/main.py
+++ pytoc/main.py
@@ -9,7 +9,10 @@ from argparse import ArgumentParser
 from prep_input_data import PrepInput
 from gen_strf import GenSTRF
 
+'''
+Variable arguments to run simulations
 
+'''
 args = ArgumentParser()
 args.add_argument('--chans', type=int, default=4, help='Number of channels')
 args.add_argument('--trials', type=int, default=10, help='Number of trials')
@@ -21,6 +24,7 @@ args.add_argument('--std', type=float, default=0.0, help='Standard deviation of 
 args.add_argument('--simlen', type=int, default=35000, help='Number of time steps (default = 35000).')
 args.add_argument('--target_dir', type=str, default='resampled-stimuli/target', help='directory containing target stimuli')
 args.add_argument('--masker_dir', type=str, default='resampled-stimuli/masker', help='directory containing masker stimuli')
+args.add_argument('--save_dir', type=str, default='save_path', help='output path')
 
     
 parsed_args = args.parse_args()
@@ -33,11 +37,15 @@ sub_config = config['input_spike_train']
 
 strf_config = config['strf_config']
 
+
+# collecting stimuli path
 lst_target_stim = [os.path.join(parsed_args.target_dir, stim_path) for stim_path in os.listdir(parsed_args.target_dir)]
 lst_masker_stim = [os.path.join(parsed_args.masker_dir, stim_path) for stim_path in os.listdir(parsed_args.masker_dir)]
 
+# initiating class object that generates STRFs
 gen_strfs = GenSTRF(parsed_args, strf_config, lst_target_stim[0])
 
+# Generating IC values for target stimuli
 fr_target_on = []
 fr_target_off = []
 for stim_path in lst_target_stim:
@@ -48,18 +56,21 @@ fr_target_on = np.concatenate(fr_target_on, axis=0)
 fr_target_off = np.concatenate(fr_target_off, axis=0)
 print(f'>>>> Number of Stimuli: {fr_target_on.shape[0]}')
 
+# Generating IC values for maskers
 fr_maskers = []
 for stim_path in lst_masker_stim:
         fr_on, fr_off = gen_strfs.process_stimulus(stim_path, strf_config['maskerlvl'], strf_config['stimGain'])
         fr_maskers.append([fr_off.transpose()])
-
 fr_maskers = np.concatenate(fr_maskers, axis=0)
 print(f'>>>> Number of trials: {fr_maskers.shape[0]}')
     
 
+# initiating class object that generates Poisson spikes
 prep_input = PrepInput(parsed_args, sub_config)
 
+# collecting masker and target setting information
 # masker_locs, target_locs = prep_input.make_grid_target_masker_locs()
+# list_locs = list(zip(masker_locs, target_locs))
 list_locs = [(None, 0), 
             (None, 1), 
             (None, 2), 
@@ -77,6 +88,8 @@ spks = prep_input.process_input_from_raw_stim(
 
 print("Generated spike train keys:", spks.keys())
 
-file_path = 'no_masker.pkl'
+if os.path.exists(parsed_args.save_dir)==False: os.makedirs(parsed_args.save_dir)
+
+file_path = os.path.join(parsed_args.save_dir, 'poisson_gen_spikes.pkl')
 with open(file_path, 'wb') as file:
     pk.dump(spks, file)
